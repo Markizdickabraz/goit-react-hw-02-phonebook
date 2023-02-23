03a0d43ddc276f1d04f61a012cba6bfb4c58c604
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2023-02-23 20:58:27 +0200

    filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,18 @@
 import React ,{Component} from "react";
 import ContactForm from "./form";
 import ContactList from "./contactList";
-
+import Filter from "./filter";
 
 class App extends Component {
 
   state = {
-    contacts: []
+    contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+    filter: ''
   }
 
   formSubmitHandler = (data) => {
@@ -20,15 +26,25 @@ class App extends Component {
     })
   }
 
+  chengeFilter = e => {
+     this.setState({ filter: e.currentTarget.value })
+  }
  
   
+  
   render() {
+    const { contacts, filter } = this.state;
+    
+    // const normalizedFilter = this.state.filter.toLowerCase();
+    // const filtredComponents = this.state.contacts.filter(contact => contact.text.toLowerCase().includes(normalizedFilter))
+
     return (
       <div>
         <h1>Phonebook</h1>
         <ContactForm onSubmit={this.formSubmitHandler} />
         <h2>Contacts</h2>
-        <ContactList items={this.state.contacts} />
+        <Filter value={filter} onChange={this.chengeFilter} />
+        <ContactList items={contacts} />
       </div>
     );
   }
