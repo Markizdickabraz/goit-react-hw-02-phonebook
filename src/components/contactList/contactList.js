55bdd3b9a33f579543a506d87e196fc900104dcd
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2023-02-26 11:37:16 +0200

    рендер за умовою контакт лист

--- src/components/contactList/contactList.js
+++ src/components/contactList/contactList.js
@@ -1,23 +1,27 @@
 import ListItem from "../listItem/listItem";
 // import { nanoid } from "nanoid";
-import PropTypes, { any } from 'prop-types';
+import PropTypes from 'prop-types';
 
 
 export default function ContactList({ items, onDeleteClick }) {
     // const id = nanoid();
-    return (
-        <ul>
-            {items.map(item => 
-            (
-                <ListItem key={item.name}
-                    name={item.name} number={item.number} deleteClick = {onDeleteClick} ></ListItem>
-            )
-                )}
-        </ul>
-    )
+
+    if (items.length) {
+        
+        return (
+            <ul>
+                {items.map(item =>
+                (
+                    <ListItem key={item.name}
+                        name={item.name} number={item.number} deleteClick = {onDeleteClick} ></ListItem>
+                )
+                    )}
+            </ul>
+        )
+    }
 }
 
-ListItem.PropTypes = {
-    item: PropTypes.objectOf(any),
+ListItem.prototype = {
+    item: PropTypes.objectOf(PropTypes.string),
     onDeleteClick: PropTypes.func
 } 
